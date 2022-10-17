8cc9b2f157bc72c37a640cafe36d6ca137968f9b
Santos van_der_Wansem <srrvdwansem@gmail.com>
2022-10-17 23:20:24 +0200

    remove bugs

--- src/components/OverlayRight.tsx
+++ src/components/OverlayRight.tsx
@@ -4,7 +4,7 @@ export default function OverlayRight () {
     return (
         <div id='overlay-right'>
             <div className='lin-bg'></div>
-            <p>webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   </p>
+            <p>webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   webontwikkelaar   -   middelbare scholier   -   programmeernerd   -   swiftie   -   sass fanboy   -   </p>
             <div className='lin-bg'></div>
         </div>
     )
